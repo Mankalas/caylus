e51a9d220e1ad5c84c0a784742cc5c6789a4b2fd
Mankalas <mankalas@gmail.com>
2013-03-26 23:05:55 +0100

    Comments on Bridge.

--- src/controller/bridge.hh
+++ src/controller/bridge.hh
@@ -17,12 +17,23 @@ namespace controller
 	class Player;
 	class Board;
 
+	/**
+	 * The board's Bridge.
+	 */
 	class Bridge : public BoardElement
 	{
 	public :
 
+		/**
+		 * Constructor.
+		 *
+		 * @param board The game's Board (in order to move the Provost).
+		 */
 		Bridge(Board & board);
 
+		/**
+		 * Clears the bridge of all markers.
+		 */
 		void clear();
 
 		/** Add a player to the bridge. Checks whether he is the first and
@@ -32,22 +43,31 @@ namespace controller
 		 */
 		void add(Player & p);
 
+		// See BoardElement.
 		virtual bool has(const Player & p) const;
 
-		// Accessors.
+		/** @name Accessors. */
+		// @{
+
 		const std::vector<Player *>& players() const;
 		std::vector<Player *>& players();
+
+		// @}
 
 		/// This is the 'case number' of the bridge, used by an active
 		/// view to communicate the player's choice.
 		static const unsigned int CASE_NUMBER = 69;
 
 	private:
+		/// Players' markers.
 		std::vector<Player *> players_;
+		/// The game's Board.
 		Board & board_;
 
+		// See BoardElement.
 		virtual void onActivate_();
 
+		// See BoardElement.
 		virtual bool canBeActivated_() const;
 	};
 }
